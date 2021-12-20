65b6a25fda1d4c1fac921ba57c4e0ac961ae76f5
mohaymen_rafi <mohaymen.rafi@gmail.com>
2021-12-20 09:31:08 +0600

    config edited

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyChjiertk4quXNv1LHHCRZYpGTn4DJB-vo',
-  authDomain: 'juno-toys.firebaseapp.com',
-  projectId: 'juno-toys',
-  storageBucket: 'juno-toys.appspot.com',
-  messagingSenderId: '592101167757',
-  appId: '1:592101167757:web:0af70fa7f546f9489d88c4',
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 export default firebaseConfig;
